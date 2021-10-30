c66e8170377392b5028ee9432159595bd9c2b0a5
katsuNakajima <40666782+katsuNakajima@users.noreply.github.com>
2021-10-30 23:17:21 +0900

    solve a

---
+++ src/bin/a.rs
@@ -0,0 +1,58 @@
+use std::{collections::HashMap, f32::MAX_EXP};
+
+#[allow(unused_macros)]
+macro_rules! parse_line {
+    ( $t:ty ) => (
+        {
+            let mut line = String::new();
+            ::std::io::stdin().read_line(&mut line).unwrap();
+            let mut iter = line.split_whitespace();
+            iter.next().unwrap().parse::<$t>().unwrap()
+        }
+    );
+
+    ( $( $t:ty), +) => (
+        {
+            let mut line = String::new();
+            ::std::io::stdin().read_line(&mut line).unwrap();
+            let mut iter = line.split_whitespace();
+            ( $(iter.next().unwrap().parse::<$t>().unwrap()),* )
+        }
+    );
+}
+
+#[allow(unused_macros)]
+macro_rules! read_line {
+    () => {{
+        let mut line = String::new();
+        ::std::io::stdin().read_line(&mut line).unwrap();
+        line.pop();
+        line
+    }};
+}
+
+#[allow(unused_macros)]
+macro_rules! parse_vec {
+    ( $t:ty ) => {{
+        let mut line = String::new();
+        ::std::io::stdin().read_line(&mut line).unwrap();
+        let iter = line.split_whitespace();
+        iter.map(|v| v.parse::<$t>().unwrap()).collect::<Vec<_>>()
+    }};
+}
+
+fn main() {
+    let s = parse_line!(String);
+    let mut map = HashMap::<char, u32>::new();
+    let mut ans = 6;
+    for c in s.chars() {
+        *map.entry(c).or_insert(0) += 1;
+    }
+    let m = map.values().max().unwrap();
+    if m == &3 {
+        ans = 1;
+    } else if m == &2 {
+        ans = 3;
+    }
+    println!("{}", ans);
+}
